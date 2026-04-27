99150ff5ba75f6de6a53942913f1e24baf5aaaf7
pavelchowdhury99 <59216690+pavelchowdhury99@users.noreply.github.com>
2026-04-27 13:16:33 +0530

    adding all other available languages and progress bar

--- resources/replacer.py
+++ resources/replacer.py
@@ -1,18 +1,58 @@
 replacer = {
-    "python": {
-        "python": "python",
-        ".py": ".py",
-    },
-    "java": {
-        "python": "java",
-        ".py": ".java",
+    "c": {
+        "python": "c",
+        ".py": ".c",
     },
     "cpp": {
         "python": "cpp",
         ".py": ".cpp",
     },
+    "csharp": {
+        "python": "csharp",
+        ".py": ".cs",
+    },
+    "dart": {
+        "python": "dart",
+        ".py": ".dart",
+    },
+    "go": {
+        "python": "go",
+        ".py": ".go",
+    },
+    "java": {
+        "python": "java",
+        ".py": ".java",
+    },
     "javascript": {
         "python": "javascript",
         ".py": ".js",
+    },
+    "kotlin": {
+        "python": "kotlin",
+        ".py": ".kt",
+    },
+    "python": {
+        "python": "python",
+        ".py": ".py",
+    },
+    "ruby": {
+        "python": "ruby",
+        ".py": ".rb",
+    },
+    "rust": {
+        "python": "rust",
+        ".py": ".rs",
+    },
+    "scala": {
+        "python": "scala",
+        ".py": ".scala",
+    },
+    "swift": {
+        "python": "swift",
+        ".py": ".swift",
+    },
+    "typescript": {
+        "python": "typescript",
+        ".py": ".ts",
     },
 }
